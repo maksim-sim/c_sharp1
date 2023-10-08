c050414ab6889a3a730e74e4d9446552d79ab82b
maksim kireev <maksimkir987@gmail.com>
2023-10-08 21:50:05 +0300

    add example 005 giri

---
+++ example005_giri/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 4;
+int c = 7;
+int d = 3;
+int e = 11;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("Максималное число: ");
+Console.WriteLine(max);
